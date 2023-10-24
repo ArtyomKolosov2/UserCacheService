fb1450daa397519a772cb3e373bed3dd460e21e9
ArtyomKolosov2 <artyomkolosov2@users.noreply.github.com>
2023-10-24 23:59:26 +0200

    Adjust .gitignore files

---
+++ UserCacheService.Infrastructure/Database/Context/UserCacheServiceDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserCacheService.Domain.UserInfo;
+using UserCacheService.Infrastructure.Authentication;
+
+namespace UserCacheService.Infrastructure.Database.Context;
+
+public class UserCacheServiceDatabaseContext : DbContext
+{
+    public virtual DbSet<UserInfo> UserInfos { get; set; }
+    
+    public virtual DbSet<BasicAuthenticationCredentials> BasicAuthenticationCredentials { get; set; }
+
+    public UserCacheServiceDatabaseContext(DbContextOptions<UserCacheServiceDatabaseContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
